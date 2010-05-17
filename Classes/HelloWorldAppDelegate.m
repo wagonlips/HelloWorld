837b67cf7a9552c2882ed53d518dd54c52b7c27c
Sean Camden <sean@fuzzysocks.(none)>
2010-05-17 16:27:03 -0700

    Added a view.

--- Classes/HelloWorldAppDelegate.m
+++ Classes/HelloWorldAppDelegate.m
@@ -6,24 +6,39 @@
 //  Copyright __MyCompanyName__ 2010. All rights reserved.
 //
 
+#import "MyViewController.h"
 #import "HelloWorldAppDelegate.h"
 
 @implementation HelloWorldAppDelegate
 
 @synthesize window;
+@synthesize myViewController;
 
-
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {    
+// The following line was written by Xcode. The line after is from the iPhone tutorial, Adding a View Controller.
+// - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {    
+- (void)applicationDidFinishLaunching:(UIApplication *)application {    
 
     // Override point for customization after application launch
+	MyViewController *aViewController = [[MyViewController alloc]
+										 
+										 initWithNibName:@"MyViewController" bundle:[NSBundle mainBundle]];
+	
+	[self setMyViewController:aViewController];
+	
+	[aViewController release];
+	
+	UIView *controllersView = [myViewController view];
+	
+	[window addSubview:controllersView];
 	
     [window makeKeyAndVisible];
 	
-	return YES;
+//	return YES;
 }
 
 
 - (void)dealloc {
+	[myViewController release];
     [window release];
     [super dealloc];
 }
---
+++ MyViewController.h
@@ -0,0 +1,16 @@
+//
+//  MyViewController.h
+//  HelloWorld
+//
+//  Created by Sean Camden on 5/17/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MyViewController : UIViewController {
+
+}
+
+@end
